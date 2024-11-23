62f34574eb43bafcbc536e0395242267afdf4fa3
Leonardo Inacio de Oliveira <inacioleozinho3@gmail.com>
2024-11-23 16:24:33 -0300

    Add files via upload

---
+++ exerciciosAula4.py
@@ -0,0 +1,55 @@
+#Atividade 1 - EXEMPLO
+print("EXEMPLO")
+nota_a = 3
+nota_b = 2
+nota_c = 7
+media = (nota_a+nota_b+nota_c)/3
+print(media)
+aprovacao = media >= 7
+print('Aprovado?',aprovacao)
+
+#Exercício 1 - Crie um programa para efetuar a leitura de um número inteiro
+#e apresentar o resultado do quadrado deste número.
+print()
+print('EXERCÍCIO 1')
+z = 4
+q = z**2
+print(q)
+
+#Exercício 2 - Crie duas variáveis para armazenar seu primeiro nome e sobrenome.
+#Em seguida, concatene-as para formar seu nome completo e exiba o resultado.
+print()
+print('EXERCÍCIO 2')
+nome = "Leonardo"
+sobrenome = "Inacio de Oliveira"
+print(nome,sobrenome)
+
+#Exercicio 3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis.
+#Realize a concatenação desses números como strings e exiba o resultado.
+print()
+print('EXERCÍCIO 3')
+a = int(input("Digite um número inteiro: "))
+b = int(input("Digite um outro número inteiro: "))
+an = str(a)
+bn = str(b)
+print(an+bn)
+
+#Exercício 4 - # Crie uma variável para armazenar a palavra "Python".
+#Em seguida, adicione um número inteiro ao final da palavra usando a
+#concatenação e exiba o resultado.
+print()
+print('EXERCÍCIO 4')
+word = "python"
+z = 1
+zn = str(z)
+print(word+zn)
+
+#Exercício 5 - #### Declare uma variável contendo uma frase.
+#Em seguida, peça ao usuário para digitar uma palavra
+#e concatene essa palavra no final da frase. Exiba o resultado.
+print()
+print('EXERCÍCIO 5')
+frase = "O São Paulo é hexacampeão brasileiro"
+complemento = str(input("Digite uma palavra/termo: "))
+print()
+print(frase,complemento)
